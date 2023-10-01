2be68c892b3787b2afc89979b42d41a39fc1d560
geekpuja <pujuagarwal5263@gmail.com>
2023-10-02 00:46:03 +0530

    cors fix

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ require("./db-connection")
 
 const app=express();
 app.use(cors({
-  origin: 'http://localhost:3000', // Allow requests from this origin
+  origin: 'https://cardio-care-frontend-git-main-pujaagarwal5263.vercel.app', // Allow requests from this origin
   credentials: true, // Allow sending cookies or authorization headers
 }));
 app.use(bodyParser.json());
