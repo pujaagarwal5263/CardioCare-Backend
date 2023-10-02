5c23a8632cc0c97d84bca60bc62a312c2e378748
geekpuja <pujuagarwal5263@gmail.com>
2023-10-02 13:28:10 +0530

    config

--- controllers/controller.js
+++ controllers/controller.js
@@ -37,7 +37,7 @@ const generateAuthURL = async (req, res) => {
 
   const authUrl = NylasCongif.urlForAuthentication({
     loginHint: body.email_address,
-    redirectURI: "https://cardiocare.netlify.app//dashboard",
+    redirectURI: "https://cardiocare.netlify.app/dashboard",
     scopes: [Scope.EmailModify, Scope.EmailSend, Scope.Calendar],
   });
 
